bf6806b304fe5ebc618868c7e58cdbd58d099e49
Fran García <fgarciadevelop@gmail.com>
2024-06-17 11:34:09 +0200

    feat: sorteo preparado

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -15,6 +15,7 @@ export class AdminComponent {
 
   participantes: Participante[] = [];
   ganadores: Participante[] = [];
+  reservas: Participante[] = [];
   showList = 'participantes';
 
   constructor() {
@@ -23,49 +24,34 @@ export class AdminComponent {
 
   ngOnInit() {
     this.loadParticipantes();
-    this.loadGanadores();
+    // this.loadGanadores();
   }
 
   loadParticipantes() {
-    this.participantes = [
-      {
-        dni: 'string',
-        nombre: 'string',
-        apellidos: 'string',
-        email: 'string',
-        telefono: 'string',
-        entrada: 'string'
-      },
-      {
-        dni: 'string',
-        nombre: 'string',
-        apellidos: 'string',
-        email: 'string',
-        telefono: 'string',
-        entrada: 'string'
-      }
-    ];
+    this.participantes = Array.from({ length: 10 }, (_, i) => ({
+      dni: Math.floor(Math.random() * 1000000000).toString().padStart(9, '0'),
+      nombre: `Nombre${i + 1}`,
+      apellidos: `Apellidos${i + 1}`,
+      email: `email${i + 1}@example.com`,
+      telefono: Math.floor(Math.random() * 1000000000).toString().padStart(9, '0'),
+      entrada: (i + 1).toString().padStart(5, '0')
+    }));
   }
 
-  loadGanadores() {
-    this.ganadores = [
-      {
-        dni: 'ganador',
-        nombre: 'ganador',
-        apellidos: 'ganador',
-        email: 'ganador',
-        telefono: 'ganador',
-        entrada: 'ganador'
-      },
-      {
-        dni: 'ganador',
-        nombre: 'ganador',
-        apellidos: 'ganador',
-        email: 'ganador',
-        telefono: 'ganador',
-        entrada: 'ganador'
-      }
-    ];
+  sortearParticipantes() {
+    // Desordenar el array de participantes
+    this.participantes.sort(() => Math.random() - 0.5);
+  
+    // Agregar los participantes al array de ganadores uno por uno
+    this.participantes.forEach((participante, index) => {
+      setTimeout(() => {
+        if (index < 5) {
+          this.ganadores.push(participante);
+        } else {
+          this.reservas.push(participante);
+        }
+      }, index * 500); // Agregar un participante cada segundo
+    });
   }
 
 }
